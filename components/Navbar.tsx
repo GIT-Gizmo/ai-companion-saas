ff5970027cfa67539e231a1c2d53154e56193a41
GIT-Gizmo <boluforjesus@gmail.com>
2025-07-08 22:17:22 +0100

    feat: integrate Clerk for authentication and add sign-in/sign-up pages
    - Add ClerkProvider to RootLayout for authentication context in layout.tsx
    - Update Navbar component to include SignInButton for unauthenticated users and UserButton for authenticated users in Navbar.tsx
    - Create sign-in page using Clerk's SignIn component in app/sign-in/page.tsx
    - Create sign-up page using Clerk's SignUp component in app/sign-up/page.tsx
    - Implement middleware for route protection using Clerk in middleware.ts
    - Update package.json and package-lock.json to include @clerk/nextjs dependency

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,12 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import NavItems from './NavItems'
+import {
+    SignInButton,
+    SignedIn,
+    SignedOut,
+    UserButton,
+} from "@clerk/nextjs";
 
 const Navbar = () => {
 
@@ -19,7 +25,14 @@ const Navbar = () => {
             </Link>
             <div className="flex items-center gap-8">
                 <NavItems />
-                <p>Sign In</p>
+                <SignedOut>
+                    <SignInButton>
+                        <button className="btn-signin">Sign In</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton afterSwitchSessionUrl='/' />
+                </SignedIn>
             </div>
         </nav>
     )
